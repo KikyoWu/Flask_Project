829155dcbdff064dbee8a57c7341631ff9d707ac
KikyoWu <67138676+KikyoWu@users.noreply.github.com>
2020-10-21 16:21:31 +0800

    Add files via upload

---
+++ shop/config.py
@@ -0,0 +1,20 @@
+# -*- coding=utf-8 -*-
+import os
+class Config:
+    SECRET_KEY = 'mrsoft'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        '''初始化配置文件'''
+        pass
+
+# the config for development
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/shop'
+    DEBUG = True
+
+# define the config
+config = {
+    'default': DevelopmentConfig
+}
